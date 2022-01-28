e3cf802b7a31b39cbb73f539f1b04fe41af6f2b8
MohamedGElsharkawy <Mohammedgammal96@gmail.com>
2022-01-28 21:51:27 +0200

    Resolve gradle dependencies conflicts

---
+++ app/src/main/java/com/example/contacts/presentation/feature/auth/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.example.contacts.presentation.feature.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.contacts.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
