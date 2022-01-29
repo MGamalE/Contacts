a13be30af13741f352f998cea85ce1a4cb399632
MohamedGElsharkawy <Mohammedgammal96@gmail.com>
2022-01-29 19:32:10 +0200

    :zap: Implement spash navigation to login screen

--- app/src/main/java/com/example/contacts/presentation/feature/splash/SplashFragment.java
+++ app/src/main/java/com/example/contacts/presentation/feature/splash/SplashFragment.java
@@ -2,16 +2,27 @@ package com.example.contacts.presentation.feature.splash;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.contacts.R;
+import com.example.contacts.databinding.FragmentSplashBinding;
 
 
 public class SplashFragment extends Fragment {
+
+    private static final int TIME_OUT = 2000;
+
+    private FragmentSplashBinding binding;
 
 
     @Override
@@ -23,6 +34,24 @@ public class SplashFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        binding = FragmentSplashBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        launchLoginUI();
+    }
+
+    private void launchLoginUI() {
+        new Handler().postDelayed(() -> Navigation.findNavController(binding.getRoot()).navigate(R.id.action_splashFragment_to_loginFragment), TIME_OUT);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
